5755ca846c330c906bfac383d80823d3a5a928f8
soybean217 <13565644@qq.com>
2017-09-07 20:38:07 +0800

    增加frame解析和log

--- UServer/jingyi/main.py
+++ UServer/jingyi/main.py
@@ -1,42 +1,4 @@
-
-import json
-from binascii import hexlify
-import requests
-from gevent import Greenlet
-from database.db1 import db1, Channel1
-from database.db0 import db0
 from database.db2 import db2
-from userver.object.addr import AddrManger
-
-from binascii import unhexlify
-from base64 import b64decode
-from datetime import datetime
-
-from .objects.device import JoiningDev, Device, ActiveMode, db_session
-from .log import logger, ConstLog
-from config import HOST
-import time
-import calendar
-
-"""
-{'request_msg': '00a0000000000000a0a000000000000000bbe5c1b77f00',
- 'dev_eui': '00000000000000a0',
- 'dev_addr': '00000002',
- 'app_eui': 'a0000000000000a0',
- 'dl_settings': '00',
- 'rx_delay': '01',
- 'cf_list': '184f84e85684b85e84886684586e8400'}
-"""
-
-# url = 'http://183.230.40.231:6100/join?net_id=22&token=eXU7UoKsfBZ5dTtSgqx8Fg'
-
-url = 'http://' + HOST + ':6100/join?net_id=22&token=eXU7UoKsfBZ5dTtSgqx8Fg'
-headers = {'content-type': 'application/json'}
-
-
-# class ListenJoinReqThreading(threading.Thread):
-#     def __init__(self):
-#         threading.Thread.__init__(self, daemon=True)
 
 
 def doConnect(host, port):
@@ -52,8 +14,8 @@ def listen_jingyi_request():
     host, port = "127.0.0.1", 12345
     # sockLocal = doConnect(host, port)
     ps = db2.pubsub()
-    # ps.subscribe("up_alarm:9999939a00000000")
-    ps.subscribe("up_alarm:1020304050607080")
+    ps.subscribe("up_alarm:9999939a00000000")
+    # ps.subscribe("up_alarm:1020304050607080")
     while True:
         for item in ps.listen():
             print(str(item))
@@ -61,7 +23,13 @@ def listen_jingyi_request():
                 print(str(item['data']))
                 dataFromDev = db2.hgetall(item['data'])
                 print(str(dataFromDev))
-                print(str(dataFromDev['data']))
+                dataFrame = dataFromDev[b'data']
+                print(str(dataFrame))
+                print(len(dataFrame))
+                firstByte = int(dataFrame[0])
+                print(firstByte)
+                frameType = (firstByte & 0b11110000) >> 4
+                print(frameType)
                 # print(str(item['data'].decode()))
                 # print(str(db2.get(item['data'].decode())))
                 # thr = Greenlet(process_join_request, data)
--- echo-server/reconnect-client.py
+++ echo-server/reconnect-client.py
@@ -5,6 +5,11 @@ import os
 import sys
 import time
 import socket
+# import crc16
+from binascii import unhexlify
+from binascii import hexlify
+import crcmod
+import codecs
 
 
 def doConnect(host, port):
@@ -15,25 +20,73 @@ def doConnect(host, port):
         pass
     return sock
 
+header = {}
+# 地磁数据报文 0x20
+# 主机状态报文 0x21
+header['type'] = b'\x20'
+header['host_code_machine_id'] = 3
+# B 代表市场展示；D 代表销售订单；E 代表测试项目
+header['host_code_sale_type'] = b'\x0d'
+header['host_code_sale_year'] = 17
+header['host_code_project_id'] = 1
+header['nowtime'] = 1
+sensorBody = {}
+sensorBody['position_id'] = 1
+sensorBody['sensor_state'] = 1
+sensorBody['park_count'] = 1
+sensorBody['voltage'] = 1
+sensorBody['reserved_field'] = 0
+heartbeatBody = {}
+heartbeatBody['alarm'] = 0
+heartbeatBody['voltage'] = 13000
+heartbeatBody['reserved'] = 0
+
+
+crc16 = crcmod.mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)
+
+
+def procHeaderer():
+    return header['host_code_machine_id'].to_bytes(4, byteorder='little') + header['host_code_project_id'].to_bytes(3, byteorder='little') + header['host_code_sale_year'].to_bytes(2, byteorder='big') + header['host_code_sale_type'] + int(round(time.time())).to_bytes(8, byteorder='little')
+
+
+def procSensor():
+    tmpHeader = b'\x20' + procHeaderer()
+    tmpBody = heartbeatBody['alarm'].to_bytes(1, byteorder='little') + sensorBody['sensor_state'].to_bytes(
+        4, byteorder='little') + sensorBody['park_count'].to_bytes(2, byteorder='little') + sensorBody['voltage'].to_bytes(1, byteorder='little') + sensorBody['reserved_field'].to_bytes(7, byteorder='little')
+    crc = crc16(tmpHeader + tmpBody)
+    return tmpHeader + tmpBody + crc.to_bytes(2, byteorder='big')
+
+
+def procHeartbeat():
+    tmpHeader = b'\x21' + procHeaderer()
+    tmpBody = sensorBody['position_id'].to_bytes(1, byteorder='little') + heartbeatBody[
+        'voltage'].to_bytes(2, byteorder='little') + heartbeatBody['reserved'].to_bytes(1, byteorder='big')
+    crc = crc16(tmpHeader + tmpBody)
+    # crc = crc16.crc16xmodem(tmpHeader)
+    return tmpHeader + tmpBody + crc.to_bytes(2, byteorder='big')
+
 
 def main():
-    host, port = "127.0.0.1", 12345
-    print host, port
+    host, port = "testgeo.bolinparking.com", 12366
     sockLocal = doConnect(host, port)
 
     while True:
         try:
-            msg = str(time.time())
+            msg = procHeartbeat()
+            msg = procSensor()
             sockLocal.send(msg)
-            print "send msg ok : ", msg
-            print "recv data :", sockLocal.recv(1024)
+            print("send msg ok : ", len(msg))
+            print("send msg ok : ", hexlify(msg).decode())
+            recv = sockLocal.recv(1024)
+            print("recv data :", recv)
+            print("recv data :", hexlify(recv).decode())
         except socket.error:
-            print "\r\nsocket error,do reconnect "
+            print("socket error,do reconnect ")
             time.sleep(3)
             sockLocal = doConnect(host, port)
-        except:
-            print '\r\nother error occur '
-            time.sleep(3)
+        # except:
+        #     print('other error occur ')
+        #     time.sleep(3)
         time.sleep(1)
 
 if __name__ == "__main__":
